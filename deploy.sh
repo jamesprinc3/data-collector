ba6a5ec3106663e7a3843faf3d6aa819559a7865
James Prince <jamesprinc3@me.com>
2018-01-31 16:03:14 +0000

    changed interval from 5 seconds to 600

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 git checkout master
 git pull
 cd ..
-python3 data-collector/ -i 5 -e gdax -v false 2> logs/gdax.log &
-python3 data-collector/ -i 5 -e bitfinex -v false 2> logs/bitfinex.log &
+python3 data-collector/ -i 600 -e gdax -v false 2> logs/gdax.log &
+python3 data-collector/ -i 600 -e bitfinex -v false 2> logs/bitfinex.log &
 current_date_time="`date +%Y%m%d%H%M%S`"
 echo "Starting data collector at $(date)";
 
