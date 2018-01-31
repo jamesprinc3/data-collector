9c311a99a76d4252fd3b8fa7d8dcb8fe92c76692
James Prince <jamesprinc3@me.com>
2018-01-31 15:58:02 +0000

    Separated exchange logic, so now we shall have 2 different processes, so that we can investigate which one is crashing. This is also better for maintaining things in the future too

--- __main__.py
+++ __main__.py
@@ -4,32 +4,12 @@ import datetime
 from bitfinex import BitfinexClient
 from gdax_client import GdaxClient
 import signal
-import sys
 import os
-import logging
 import log
 import time
+import argparse
 
 if __name__ == '__main__':
-
-    logger = log.setup_custom_logger(__name__)
-    logger.info('Program started')
-
-    interval = datetime.timedelta(minutes=10)
-    if len(sys.argv) == 3:
-        interval = datetime.timedelta(seconds=int(sys.argv[1]))
-        should_disable_logs = sys.argv[2] == "True"
-        # if should_disable_logs:
-            # logger = logging.getLogger()
-            # logger.setLevel(logging.WARNING)
-
-    print("Interval is " + str(interval.seconds) + " seconds")
-    # execute only if run as the entry point into the program
-    bitfinex = BitfinexClient(interval)
-    gdax = GdaxClient(interval)
-
-    bitfinex.start()
-    gdax.start()
 
     def interrupt_handler(signal, frame):
         print('You pressed Ctrl+C!')
@@ -41,26 +21,56 @@ if __name__ == '__main__':
         gdax.close()
         # print("aqui 3")
 
-    signal.signal(signal.SIGINT, interrupt_handler)
 
-    pidfile_path = "/tmp/data-collector.pid"
+    def resolve_pid(exchange):
+        pidfile_path = "/tmp/data-collector-" + exchange + ".pid"
 
-    if os.path.isfile(pidfile_path):
-        pidfile = open(pidfile_path, 'r')
-        other_pid = pidfile.read()
+        if os.path.isfile(pidfile_path):
+            pidfile = open(pidfile_path, 'r')
+            other_pid = pidfile.read()
+            pidfile.close()
+
+            logger.info("previous process pid: ", other_pid)
+            try:
+                os.kill(int(other_pid), signal.SIGINT)
+            except:
+                logger.info("previous process doesn't exist")
+
+        my_pid = str(os.getpid())
+        pidfile = open(pidfile_path, 'w')
+        pidfile.write(my_pid)
         pidfile.close()
 
-        logger.info("previous process pid: ", other_pid)
-        try:
-            os.kill(int(other_pid), signal.SIGINT)
-        except:
-            logger.info("previous process doesn't exist")
+    logger = log.setup_custom_logger(__name__)
+    logger.info('Program started')
 
-    my_pid = str(os.getpid())
-    pidfile = open(pidfile_path, 'w')
-    pidfile.write(my_pid)
-    pidfile.close()
+    parser = argparse.ArgumentParser(description='Collect crypto exchange data')
+    parser.add_argument('-e', '--exchange', nargs='?',
+                        help='<Required> Exchange to listen to', required=True)
+    parser.add_argument('-v', '--verbose', nargs='?',
+                        help='Verbosity, default: false')
+    parser.add_argument('-i', '--interval', nargs='?',
+                        help='Interval between saving incoming messages', type=int, required=True)
+
+    args = parser.parse_args()
+    print(args)
+
+    interval = datetime.timedelta(seconds=int(args.interval))
+
+    print("Interval is " + str(interval.seconds) + " seconds")
+    # execute only if run as the entry point into the program
+    bitfinex = BitfinexClient(interval)
+    gdax = GdaxClient(interval)
+
+    if "bitfinex" == args.exchange:
+        resolve_pid("bitfinex")
+        bitfinex.start()
+
+    if "gdax" == args.exchange:
+        resolve_pid("gdax")
+        gdax.start()
+
+    signal.signal(signal.SIGINT, interrupt_handler)
 
     while True:
         time.sleep(5)
-
